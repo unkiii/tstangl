abb24dff602b8b063ea5917e04291f4f7ac91260
INDRA\fnavarrov <fnavarrov@indra.es>
2019-05-07 11:55:42 +0200

    runs

--- src/app/components/pagines/tercer/tercer.component.ts
+++ src/app/components/pagines/tercer/tercer.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'tercer',
     templateUrl: 'tercer.component.html',
     styleUrls: ['tercer.component.scss']
